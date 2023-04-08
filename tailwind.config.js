35f924ac73bde0de868d085cb2cc0efde1304c5a
BAHROMTJ <sarifzodabahromofficall@gmail.com>
2023-04-08 15:50:47 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'h': '300px',
+      'p': '400px',
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui',],
+      'serif': ['ui-serif', 'Georgia',],
+      'mono': ['ui-monospace', 'SFMono-Regular',],
+      'display': ['Oswald',],
+      'body': ['"Open Sans"', ],
+    },
+  gridTemplateColumns: {
+      'fill-40': 'repeat(auto-fitt, 300px)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
